e13b574ecf89b3e37877526e1682a95073076e0e
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2023-04-02 17:24:24 +0200

    Root for multi project

---
+++ Android/libs/build/generated/source/buildConfig/debug/it/giaquinto/libs/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.giaquinto.libs;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "it.giaquinto.libs";
+  public static final String BUILD_TYPE = "debug";
+}
